f95f7595c73f8e5999f70ae1c064f838417b778e
DOMINICKALUMBA <dominiquekalumba1000@gmail.com>
2024-08-20 16:19:59 +0300

    learning for loops

---
+++ loops-and-array.js
@@ -0,0 +1,12 @@
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+/* Use a for loop to log the following to the console:
+
+The 5 largest countries in the world:
+- China
+- India
+- United States
+- Indinesia
+- Pakistan
+*/
+for(let j = 0; j < largeCountries.length; j++){console.log(largeCountries)}
