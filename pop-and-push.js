41c4a3ec47b089591622079aa4ba4e16882b3672
DOMINICKALUMBA <dominiquekalumba1000@gmail.com>
2024-08-20 16:55:56 +0300

    pop,push,shift and unshift

---
+++ pop-and-push.js
@@ -0,0 +1,27 @@
+//let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+//largeCountries.pop()
+//let addF = "china"pakistan";
+//largeCountries.push("pakistan")
+
+//largeCountries.shift()
+//largeCountries.unshift(china) 
+//console.log(largeCountries)
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+largeCountries.pop()
+largeCountries.push("Pakistan")
+largeCountries.shift()
+largeCountries.unshift("China")
+console.log(largeCountries)
