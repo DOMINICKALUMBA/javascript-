b879d868cc8460b4c85b2d6ae247c2db9bc891cf
DOMINICKALUMBA <dominiquekalumba1000@gmail.com>
2024-09-29 13:47:56 +0300

    functions

--- function.js
+++ function.js
@@ -6,3 +6,15 @@ function average(av1,av2) {
 }
 const data = average(130,110);
 console.log(data)
+
+
+//function that log out even number between 0 to 100
+function numbers(){
+    for (let i = 0; i <= 100; i++) {
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+        
+    }
+}
+numbers()
