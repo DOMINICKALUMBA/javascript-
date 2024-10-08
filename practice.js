272a9c40694eb2e071a8b77b061d83d2938400bb
DOMINICKALUMBA <dominiquekalumba1000@gmail.com>
2024-10-08 11:32:50 +0300

    functions

--- practice.js
+++ practice.js
@@ -1,15 +1,42 @@
 
-//for (let i = 1; i <= 100; i++) {
-  //  console.log(i);
-//}
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
 
 
 //for(let evenNo = 2; evenNo <= 50; evenNo++) {console.log(evenNo);}
 //if(evenNo % 2 === 0){console.log(evenNo);}
 
 
-for(let me = 0; me < 150; me++){if (me  % 2 === 0 ) {
+for(let me = 0; me < 100; me++){if (me  % 2 === 0 ) {
     
 }
     console.log(me)
 }
+
+//in a function way
+
+function num() {
+  
+  for (let ie = 0; ie <= 100; ie++) {
+    if (ie % 2 === 0) {
+      console.log(ie)
+    }
+    
+  }
+}
+num()
+
+
+
+//anothe way
+function num(limit) {
+  for (let ie = 0; ie <= limit; ie++) {
+    if (ie % 2 === 0) {
+      console.log(ie); // Log even numbers
+    }
+  }
+}
+
+// Call the function with an argument
+num(50); // This will print even numbers up to 50
