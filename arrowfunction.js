7bd14b08b2ce58c4e89ddf6380d7497c78987d29
DOMINICKALUMBA <dominiquekalumba1000@gmail.com>
2024-11-27 12:43:01 +0300

    squre of numbers

--- arrowfunction.js
+++ arrowfunction.js
@@ -3,9 +3,11 @@ function sum(s1,s2) {
       return  st     
     }
 
-console.log(sum(2,3));
 
 
 //arrow function
 const add = (a,b) => a + b
 console.log(add(2,3));
+
+const sq = (a) => a * a
+console.log(sq(2));
